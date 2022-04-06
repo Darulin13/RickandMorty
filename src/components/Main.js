8b178a3a3b51118ce2d9b916ea50b41afe8aed7a
João Ferreira 13 <joaop338328@gmail.com>
2022-04-06 18:59:49 -0300

    Main working

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,50 @@ import styled from "styled-components";
 import axios from "axios";
 
 const Container = styled.main`
-width:100%;
-border:solid;
+    width:100%;
+    border:solid;
     background-color: rgb(60, 62, 68);  
+    display:flex;
+    flex-direction:column;
+    justify-content:space-evenly;
+    align-items:center;
+    height:450vh;
 
+    div{
+        width:95%;  
+        display:grid;
+        grid-template-columns:50% 50%;
+        row-gap: 20px;
+        align-content: center;
+        justify-items: center
+    }
+    @media(max-width:1200px){
+        display:grid;
+        grid-template-columns:1fr;
+        
+    }
     `
+
+const Box = styled.section`
+    width:95%;
+    border:solid white;
+    display:flex;
+    flex-direction:row;
+    color:white;
+    border-radius:8px;
+
+    img{
+ 
+        width:40%;
+     
+
+    }
+`
+const Info = styled.section`
+  
+`
 const api = axios.create({
-    baseURL: " https://rickandmortyapi.com/api/character"
+    baseURL: " https://rickandmortyapi.com/api/character/?page=19"
 })
 export default class Main extends React.Component {
     state = {
@@ -20,9 +57,9 @@ export default class Main extends React.Component {
         const response = await api.get()
         console.log("Main", response.data.results)
 
-        const characters = response.data.resuts.map((item) => {
+        const characters = response.data.results.map((item) => {
             return {
-                ...item
+                ...item,
             }
         })
         this.setState({
@@ -33,12 +70,32 @@ export default class Main extends React.Component {
     render() {
         return (
             <Container>
-                {this.state.listCharacters.map((item) => (
-                    <div key={item.id}><p>{item.name}</p></div>
-                    
-                ))
+                <div>
+                    {this.state.listCharacters.map((item) => (
+                        <Box>
+                            <img src={item.image} alt={item.name} />
+                            <Info>
+                                <ul>
+                                    <h1>{item.name}</h1>
+                                    <li>{item.status} - {item.species}</li>
+                                </ul>
+                                <article>
+                                    <h2>Last known location:</h2>
+                                    <p>{item.origin.name}</p>
+                                </article>
+                                <article>
+                                    <h2>First seen in:</h2>
+                                    <p> {item.location.name}</p>
+                                </article>
 
-                }
+                            </Info>
+                        </Box>
+
+
+                    ))
+
+
+                    }       </div>
 
             </Container>
         )
